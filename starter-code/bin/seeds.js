950ec7c56a29625624e4ba6deb5f33051423e316
Javier Samper <jsamperarias@hotmail.com>
2018-11-26 20:13:47 +0100

    seeds

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -10,38 +10,57 @@ const User = require("../models/User");
 const bcryptSalt = 10;
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect('mongodb://localhost/starter-code', { useNewUrlParser: true })
+    .then(x => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err)
+    });
 
-let users = [
-  {
-    username: "alice",
-    password: bcrypt.hashSync("alice", bcrypt.genSaltSync(bcryptSalt)),
-  },
-  {
-    username: "bob",
-    password: bcrypt.hashSync("bob", bcrypt.genSaltSync(bcryptSalt)),
-  }
-]
+
+let generateUser = {
+    username: 'stevenzzzzz',
+    password: bcrypt.hashSync('steven', bcrypt.genSaltSync(bcryptSalt)),
+    email: 'steven@gmail.com',
+    name: 'steven',
+    lastName: 'Ruiz'
+}
+
+let generatePost = {
+    title: 'proyecto',
+    content: 'contenido'
+}
+
+let generateComments = {
+    title: 'titulo',
+    content: 'contenido titulo',
+    comments: 'comentario'
+}
+
+let generateNews = {
+    title: 'Proyecto',
+    description: 'descripcion de proyecto',
+    content: 'contenido',
+    date: 'fecha',
+    image: 'imagen'
+}
+
+
 
 User.deleteMany()
-.then(() => {
-  return User.create(users)
-})
-.then(usersCreated => {
-  console.log(`${usersCreated.length} users created with the following id:`);
-  console.log(usersCreated.map(u => u._id));
-})
-.then(() => {
-  // Close properly the connection to Mongoose
-  mongoose.disconnect()
-})
-.catch(err => {
-  mongoose.disconnect()
-  throw err
-})
+    .then(() => {
+        return User.create(users)
+    })
+    .then(usersCreated => {
+        console.log(`${usersCreated.length} users created with the following id:`);
+        console.log(usersCreated.map(u => u._id));
+    })
+    .then(() => {
+        // Close properly the connection to Mongoose
+        mongoose.disconnect()
+    })
+    .catch(err => {
+        mongoose.disconnect()
+        throw err
+    })
