f3d70193cd1b59fbaa15d8966536fe3bea7ea613
Javier Samper <jsamperarias@hotmail.com>
2018-11-26 20:20:10 +0100

    change index

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+//MODELS
+const Comments = require("../models/Comments");
+const Post = require("../models/Post");
+const User = require("../models/User");
+const News = require("../models/News");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
+
+router.get('/delete/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id)
+        .then(x =>
+            Comments.remove({ author: req.params.id }, function(err, data) {
+                if (err) {
+                    res.send("Error");
+                } else {
+                    res.redirect('/')
+                }
+            }),
+        )
+        .catch(err => console.log(err));
+})
 
 module.exports = router;
