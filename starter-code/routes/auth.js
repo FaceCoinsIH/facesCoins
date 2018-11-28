db8d5301eabc72dd565b33d5bfaf6bb2a74fdbde
Javier Samper <jsamperarias@hotmail.com>
2018-11-28 12:10:03 +0100

    Inicio maquetacion coin.hbs

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -13,72 +13,67 @@ const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 router.get("/login", ensureLoggedOut(), (req, res, next) => {
-  res.render("auth/login", { message: req.flash("error") });
+    res.render("auth/login", { message: req.flash("error") });
 });
 
 router.post(
-  "/login",
-  ensureLoggedOut(),
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/auth/login",
-    failureFlash: true,
-    passReqToCallback: true
-  })
+    "/login",
+    ensureLoggedOut(),
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/auth/login",
+        failureFlash: true,
+        passReqToCallback: true
+    })
 );
 
-router.get("/signup", ensureLoggedOut(),(req, res, next) => {
-  res.render("auth/signup");
+router.get("/signup", ensureLoggedOut(), (req, res, next) => {
+    res.render("auth/signup");
 });
 
 
-router.post("/signup",ensureLoggedOut(), (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const email = req.body.email;
+router.post("/signup", ensureLoggedOut(), (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
 
-  if (username === "" || password === "" || email === "") {
-    res.render("auth/signup", { message: "Please fully complete the form" });
-    return;
-  }
-
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
+    if (username === "" || password === "" || email === "") {
+        res.render("auth/signup", { message: "Please fully complete the form" });
+        return;
     }
 
-    User.findOne({ email }, "email", (err, email) => {
-      if (email !== null) {
-        res.render("auth/signup", { message: "The email already exists" });
-        return;
-      }
-    });
-
-
+    User.findOne({ username }, "username", (err, user) => {
+        if (user !== null) {
             res.render("auth/signup", { message: "The username already exists" });
             return;
         }
 
+        User.findOne({ email }, "email", (err, email) => {
+            if (email !== null) {
+                res.render("auth/signup", { message: "The email already exists" });
+                return;
+            }
+        });
 
-    const newUser = new User({
-      username,
-      password: hashPass,
-      email: email
+
+
+        const newUser = new User({
+            username,
+            password: hashPass,
+            email: email
+        });
+
+        newUser.save((err) => {
+            if (err) { next(null, false, { message: newUser.errors }) }
+            res.redirect("/");
+
+        });
     });
-
-    newUser.save((err) => {
-      if (err){ next(null, false, { message: newUser.errors }) }
-      res.redirect("/");
-
-    });
-  });
 });
 
 router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+    req.logout();
+    res.redirect("/");
 });
 
 module.exports = router;
-
